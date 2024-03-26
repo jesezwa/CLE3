013b6bdc8c448816642f3b57d2ae4f7bdca72e28
jesezwa <jessevdzwan@gmail.com>
2024-03-26 11:56:22 +0100

    Setup

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,32 @@
 window.addEventListener('load', init);
 
+let userLocation;
+let button;
+
 function init(){
-    const button =
+    // Benodigheden ophalen uit HTML
+    userLocation = document.getElementById('userLocation');
+    button = document.getElementById('button');
+
+    // De button verwijzen naar getGeoLocation functie
+    button.addEventListener('click', getGeoLocation);
+
+
+}
+
+
+function getGeoLocation(e){
+    // Haal locatie op en stuur door naar showlocaton functie
+    navigator.geolocation.getCurrentPosition(showLocation);
+
+    // Zorg dat de locatie continu opnieuw wordt ververst
+    navigator.geolocation.watchPosition(showLocation);
+}
+
+function showLocation(location){
+    button.remove();
+    userLocation = '';
+    userLocation.innerHTML = (location.coords.latitude + ', ' + location.coords.longitude);
+    console.log(location.coords.latitude + ', ' + location.coords.longitude)
+
 }
